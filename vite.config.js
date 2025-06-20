25025c58488e8fc230bae600c47472a463f7208d
MATEUS GUILHERME FUINI <59875329+mateusfuini@users.noreply.github.com>
2025-06-20 16:37:15 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
+  base: '/mateusfuini.github.io/', // Adicione esta linha
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
